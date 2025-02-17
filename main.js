ff09b4fe0705298b9234a9723ff825cd8544eae7
abdanila <absatarov7405@mail.ru>
2025-02-17 17:03:53 +0500

    бургер-меню версия 1

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ const scrollTop = document.pageYOffset || document.documentElement.scrollTop;
     }
     else{
         headerEl.style.backgroundColor = 'transparent';
-
     }
+  })
+
+  const bmBtn = document.querySelector('.header__content__bm__btn');
+  const headerContent = document.querySelector('.header__content');
+  const headerContentBm = document.querySelector('.header__content__bm');
+  const bmBtnClose = document.querySelectorAll('.bm__line');
+
+
+  bmBtn.addEventListener("click", function(){
+    headerContent.style.position = "absolute";
+    headerContent.style.paddingRight = "0px";
+    headerContentBm.classList.toggle("none");
+    bmBtnClose.forEach(el=>{el.classList.toggle("close");})
   })
